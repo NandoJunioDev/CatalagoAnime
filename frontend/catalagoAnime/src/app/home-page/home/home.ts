1f48ccf8462b4e94837736797a10cca43f1b36e7
NandoJunioDev <frnndmaz_cf@outlook.com>
2026-03-14 22:39:34 -0300

    fix: add, update desing in cards, and animes

--- frontend/catalagoAnime/src/app/home-page/home/home.ts
+++ frontend/catalagoAnime/src/app/home-page/home/home.ts
@@ -15,38 +15,35 @@ export class Home implements OnInit {
 
   private animeservice = inject(Animeservice); // injetamos o service
 
-  private route = inject(ActivatedRoute); // usamos activated route para observar a rota
-
-  animes:Anime[] = []; // array do tipo anime, que é o model que definimos, e aqui vamos lista a resposta da api as lista de animes e depois usar ela no html
-   private animeservice = inject(Animeservice); // injençao do service
   private route = inject(ActivatedRoute);// injenção do AcivatedRouted, quer vai o observar a rota
   animes:Anime[] = []; // array do tipo Anime, o Anime é model dos dados
 
 
 
   ngOnInit():void {
-<<<<<<< HEAD
+
     // aqui chamoos o service o hook inicializador, e dentro dele fazemos a chamada no service, e da mos subscribe para pegar a resposta, e dentro do subscribe pegamos a resposta e colocamos na variavel animes, que é um array de anime, e dentro do response tem uma propriedade data, que é onde esta a lista de animes, entao pegamos essa lista e colocamos na variavel animes, para depois usar ela no html.
 
     this.animeservice.listarAnimes().subscribe((response) => {
       this.animes = response.data;
-=======
+
 
 // aqui chamos o service e funçao que ira, realizar as consulta
 // o .subscribe serve para ligar a a funçao
-// reponse sera a variavel, auxiliar 
-    this.animeservice.listarAnimes().subscribe(response => { 
+// reponse sera a variavel, auxiliar
+    this.animeservice.listarAnimes().subscribe(response => {
 
-      // aqui atribuimos o o valor a variavel que sebera a o id atraves dos dados que vao chegar 
+      // aqui atribuimos o o valor a variavel que sebera a o id atraves dos dados que vao chegar
 // route(o instancia do activedrouter), .snapshot( funçao na qual pegamos o valor atual da rota)
       const idDaRota = this.route.snapshot.paramMap.get('id');
       console.log('ID da rota:', idDaRota);
->>>>>>> 1d76176bf021c25d805fba6259d016fe75677706
+
 
 
        // aqui pegamos a resposta da api, e colocamos na variavel animes, que é um array de anime, e dentro do response tem uma propriedade data, que é onde esta a lista de animes, entao pegamos essa lista e colocamos na variavel animes, para depois usar ela no html.
 
     });
   }
-
+)}
 }
+
