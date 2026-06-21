1c59d083cff53c3061085e04fbe820cc233eab33
NandoJunioDev <frnndmaz_cf@outlook.com>
2026-06-21 15:07:09 -0300

    fix:synops add

--- backend/src/main/java/nandojuniodev/catalagoanime/dtos/AnimeDto.java
+++ backend/src/main/java/nandojuniodev/catalagoanime/dtos/AnimeDto.java
@@ -6,5 +6,5 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import nandojuniodev.catalagoanime.dtos.response.ImagesDto;
 
 
-public record AnimeDto(String title, ImagesDto images,  @JsonProperty("mal_id") Integer malId) {
+public record AnimeDto(String title, ImagesDto images,  @JsonProperty("mal_id") Integer malId,String synopsis)   {
 }
